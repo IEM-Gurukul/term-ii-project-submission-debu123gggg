e60d057483e02528bdca9a8228510660db2d7a55
Debdatta Roychoudhury <debdattaroychoudhury831@gmail.com>
2026-03-27 20:43:05 +0000

    main

---
+++ EventManagementSystem/src/Main.java
@@ -0,0 +1,13 @@
+import controller.EventController;
+import javax.swing.SwingUtilities;
+import view.EventView;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            EventView view = new EventView();
+            new EventController(view);
+            view.setVisible(true);
+        });
+    }
+}
