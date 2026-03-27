d9246e9afd87efe17dc7dd957ebedf2788edac4b
Debdatta Roychoudhury <debdattaroychoudhury831@gmail.com>
2026-03-27 20:43:32 +0000

    event

---
+++ EventManagementSystem/src/model/Event.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Event {
+    private String eventName;
+    private String eventDate;
+    private String eventLocation;
+
+    public Event(String eventName, String eventDate, String eventLocation) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.eventLocation = eventLocation;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public String getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setEventLocation(String eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+}
